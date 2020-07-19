dbbb4768b82e9a980345cbc8a7c5d8ff97ee66fb
Piotrenewicz <piotrekm9@onet.eu>
2020-07-19 16:55:22 +0200

    mode 0 and mode 1 fully done

--- main/controller_transfer_ex/controller_transfer_ex.ino
+++ main/controller_transfer_ex/controller_transfer_ex.ino
@@ -13,11 +13,11 @@ struct SEND_DATA_STRUCTURE {
   float len_a;
   float deg_a;
 
-  float grip;
-  float twist;
+  int16_t grip;      // 0 -- 1024
+  int16_t twist;     // 0 -- 1024
 
-  float drive;
-  float turn;
+  int16_t drive;   // -512 -- 0 -- 512
+  int16_t turn;    // -512 -- 0 -- 512
 
   bool drive_power;
   bool arm_power;
@@ -40,26 +40,27 @@ void setup() {
 
 
 void initialize_command() {
+  init_target();
+  
+  command.grip = 0.0;
+  command.twist = 0.0;
+
+  command.drive = 0;
+  command.turn = 0;
+
+  command.arm_power = false;
+}
+
+void init_target(){
   command.target_x = -10.0;
   command.target_y = 5.0;
   command.len_a = 0.0;
   command.deg_a = 0.0;
-
-  command.grip = 0.0;
-  command.twist = 0.0;
-
-  command.drive = 0.0;
-  command.turn = 0.0;
-
-  command.drive_power = true;
-  command.arm_power = false;
-
-  command.custom = 0;
 }
 
 
 void loop() {
-
+  primary_control_junction();
   ET.sendData();
   delay(120);
 }
@@ -67,18 +68,18 @@ void loop() {
 int menu_mode = 0;
 /*
  * Mode 0 - Drive mode, 
- *  joystick will control driving, (no input will mean a detach)
- *  slider will control grip
- *  BL will set arm to default pos (grip not included)
- *  BR will send custom 1 (detach arm)
- *  BR again will send custom 2 (attach arm)
- *  BU will change mode to 1
+ *  joystick will control driving, (no input will mean a detach) ^
+ *  slider will control grip ^
+ *  BL will set arm to default pos (grip not included) ^
+ *  BR will send custom 1 (detach arm) ^
+ *  BR again will send custom 2 (attach arm) ^
+ *  BU will change mode to 1 ^
  *  
  * Mode 1 - Basic arm mode,
- *  joystick will move the target, xy,
- *  slider will control grip
- *  BU will change mode to 0
- *  BD will change mode to 2
+ *  joystick will move the target, xy, ^
+ *  slider will control grip ^
+ *  BU will change mode to 0 ^
+ *  BD will change mode to 2 ^
  *  
  * Mode 2 - Full arm mode, (led on esplora is lit in this mode)
  *  slider does nothing, this allows it to be prepared for custom
@@ -99,26 +100,94 @@ int menu_mode = 0;
 
 void primary_control_junction(){
   if(menu_mode == 0){
-    delay(10);
+    mode0();
   }
   else if(menu_mode == 1){
-    delay(10);
+    mode1();
   }
   else if(menu_mode == 2){
     secondary_control_junction();
   }
 
+  
   // data send, perhaps check for changes, maybe a changes flag from the deeper?
 }
 
 void secondary_control_junction(){
   if(not Esplora.readButton(SWITCH_LEFT)){
-    delay(10);
+    mode3b();
   }
   else if (not Esplora.readButton(SWITCH_RIGHT)){
-    delay(10);
+    mode3c();
   }
   else{
-    delay(10);
+    mode3a();
   }
+}
+
+void joytodrive(){
+  command.turn = Esplora.readJoystickX();
+  command.drive = Esplora.readJoystickY();
+}
+
+void slidetogrip(){
+  command.grip = Esplora.readSlider();
+}
+
+void joytotarget(){
+  command.target_x += map(Esplora.readJoystickX(), -512, 512, -1.0, 1.0);
+  command.target_y += map(Esplora.readJoystickY(), -512, 512, -1.0, 1.0);
+}
+
+void mode0(){
+  if(!Esplora.readButton(SWITCH_LEFT)){
+    while(!Esplora.readButton(SWITCH_LEFT)){}
+    init_target();
+  }
+
+  else if(!Esplora.readButton(SWITCH_RIGHT)){
+    while(!Esplora.readButton(SWITCH_RIGHT)){}
+    command.arm_power = !command.arm_power;
+  }
+
+  else if(!Esplora.readButton(SWITCH_UP)){
+    while(!Esplora.readButton(SWITCH_UP)){}
+    menu_mode = 1;
+  }
+
+  else {
+    digitalWrite(13, LOW);
+    joytodrive();
+    slidetogrip();
+  }
+}
+
+void mode1(){
+  if(!Esplora.readButton(SWITCH_UP)){
+    while(!Esplora.readButton(SWITCH_UP)){}
+    menu_mode = 0;
+  }
+
+  else if(!Esplora.readButton(SWITCH_DOWN)){
+    while(!Esplora.readButton(SWITCH_DOWN)){}
+    menu_mode = 2;
+  }
+  
+  else {
+    digitalWrite(13, HIGH);
+    joytotarget();
+    slidetogrip();
+  }
+}
+
+void mode3a(){
+
+}
+
+void mode3b(){
+
+}
+
+void mode3c(){
+
 }
